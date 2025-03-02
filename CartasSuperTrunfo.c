b09b3543b4affb2a6b225c8ddc5010dd2771a5a9
maykelesoares <83615381+maykelesoares@users.noreply.github.com>
2025-03-02 01:51:05 +0000

    Atualização intermediário

--- CartasSuperTrunfo.c
+++ CartasSuperTrunfo.c
@@ -1,7 +1,7 @@
 #include <stdio.h>
 
 //Teste Maykele
-//Nível Novato;
+//Nível Aventureiro;
 
 int main() {
     printf("Desafio Cartas Super Trunfo!\n");
@@ -17,32 +17,32 @@ int main() {
     
 
     // retirei o & antes das variáveis tipo char pois estava dando erro
-    printf("Digite o estado: \n");
+    printf("Digite uma letra de 'A' a 'H' que representa o estado: \n");
     scanf( "%s", estado);
 
-    printf("Digite o código da carta: \n");
+    printf("Digite a letra do estado seguida de um número de 01 a 04: \n");
     scanf( "%s", codigo_carta);
 
     printf("Digite o nome da cidade: \n");
     scanf("%s", nome);
 
     
-    printf("Digite a população: \n");
+    printf("Digite o número de habitantes da cidade: \n");
     scanf("%d", &populacao);
 
-    printf("Digite a área territorial: \n");
+    printf("Digite a área da cidade em quilômetros quadrados: \n");
     scanf("%f", &area);
 
-    printf("Digite o PIB: \n");
+    printf("Digite o Produto Interno Bruto da cidade: \n");
     scanf("%f", &pib);
 
     printf("Digite a quantidade de pontos turísticos: \n");
     scanf("%d", &pontos_turisticos);
 
     float densidade = populacao / area;
-    float pib_per_capita = pib / populacao;
+    float pib_per_capita = pib / (float)populacao;
 
-    printf(" Estado: %s. \n Código da carta: %s. \n Cidade: %s. \n População: %d. \n Área Territorial: %f. \n Densidade Populacional: %f. \n PIB: %f. \n PIB per Capita: %f. \n Pontos Turísticos: %d. \n", 
+    printf(" DADOS DA CARTA \n Estado: %s. \n Código da carta: %s. \n Cidade: %s. \n População: %d habitantes. \n Área Territorial: %.2f km². \n Densidade Populacional: %.2f hab/km². \n PIB: %.2f bilhões de reais. \n PIB per Capita: %.2f reais. \n Pontos Turísticos: %d. \n", 
         estado, codigo_carta, nome, populacao, area, densidade, pib, pib_per_capita, pontos_turisticos);
 
     return 0;
